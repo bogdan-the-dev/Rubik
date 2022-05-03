4d322b64d2afa902943a02bdac2bf2925c214c74
Bogdan <bogdantamba55@gmail.com>
2022-05-03 22:56:03 +0300

    initial

---
+++ OpenCVApplication.cpp
@@ -0,0 +1,318 @@
+// OpenCVApplication.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "common.h"
+
+
+void testOpenImage()
+{
+	char fname[MAX_PATH];
+	while(openFileDlg(fname))
+	{
+		Mat src;
+		src = imread(fname);
+		imshow("image",src);
+		waitKey();
+	}
+}
+
+
+void testParcurgereSimplaDiblookStyle()
+{
+	char fname[MAX_PATH];
+	while (openFileDlg(fname))
+	{
+		Mat src = imread(fname, IMREAD_GRAYSCALE);
+		int height = src.rows;
+		int width = src.cols;
+		Mat dst = src.clone();
+
+		double t = (double)getTickCount(); // Get the current time [s]
+
+		// the fastest approach using the “diblook style”
+		uchar *lpSrc = src.data;
+		uchar *lpDst = dst.data;
+		int w = (int) src.step; // no dword alignment is done !!!
+		for (int i = 0; i<height; i++)
+			for (int j = 0; j < width; j++) {
+				uchar val = lpSrc[i*w + j];
+				lpDst[i*w + j] = 255 - val;
+			}
+
+		// Get the current time again and compute the time difference [s]
+		t = ((double)getTickCount() - t) / getTickFrequency();
+		// Print (in the console window) the processing time in [ms] 
+		printf("Time = %.3f [ms]\n", t * 1000);
+
+		imshow("input image",src);
+		imshow("negative image",dst);
+		waitKey();
+	}
+}
+
+void testColor2Gray()
+{
+	char fname[MAX_PATH];
+	while(openFileDlg(fname))
+	{
+		Mat src = imread(fname);
+
+		int height = src.rows;
+		int width = src.cols;
+
+		Mat dst = Mat(height,width,CV_8UC1);
+
+		// Asa se acceseaaza pixelii individuali pt. o imagine RGB 24 biti/pixel
+		// Varianta ineficienta (lenta)
+		for (int i=0; i<height; i++)
+		{
+			for (int j=0; j<width; j++)
+			{
+				Vec3b v3 = src.at<Vec3b>(i,j);
+				uchar b = v3[0];
+				uchar g = v3[1];
+				uchar r = v3[2];
+				dst.at<uchar>(i,j) = (r+g+b)/3;
+			}
+		}
+		
+		imshow("input image",src);
+		imshow("gray image",dst);
+		waitKey();
+	}
+}
+
+void testBGR2HSV()
+{
+	char fname[MAX_PATH];
+	while (openFileDlg(fname))
+	{
+		Mat src = imread(fname);
+		int height = src.rows;
+		int width = src.cols;
+
+		// Componentele d eculoare ale modelului HSV
+		Mat H = Mat(height, width, CV_8UC1);
+		Mat S = Mat(height, width, CV_8UC1);
+		Mat V = Mat(height, width, CV_8UC1);
+
+		// definire pointeri la matricele (8 biti/pixeli) folosite la afisarea componentelor individuale H,S,V
+		uchar* lpH = H.data;
+		uchar* lpS = S.data;
+		uchar* lpV = V.data;
+
+		Mat hsvImg;
+		cvtColor(src, hsvImg, COLOR_BGR2HSV);
+
+		// definire pointer la matricea (24 biti/pixeli) a imaginii HSV
+		uchar* hsvDataPtr = hsvImg.data;
+
+		for (int i = 0; i<height; i++)
+		{
+			for (int j = 0; j<width; j++)
+			{
+				int hi = i*width * 3 + j * 3;
+				int gi = i*width + j;
+
+				lpH[gi] = hsvDataPtr[hi] * 510 / 360;		// lpH = 0 .. 255
+				lpS[gi] = hsvDataPtr[hi + 1];			// lpS = 0 .. 255
+				lpV[gi] = hsvDataPtr[hi + 2];			// lpV = 0 .. 255
+			}
+		}
+
+		imshow("input image", src);
+		imshow("H", H);
+		imshow("S", S);
+		imshow("V", V);
+
+		waitKey();
+	}
+}
+
+void testVideoSequence()
+{
+	VideoCapture cap("Videos/rubic.avi"); // off-line video from file
+	//VideoCapture cap(0);	// live video from web cam
+	if (!cap.isOpened()) {
+		printf("Cannot open video capture device.\n");
+		waitKey(0);
+		return;
+	}
+		
+	Mat edges;
+	Mat frame;
+	char c;
+
+	while (cap.read(frame))
+	{
+		Mat grayFrame;
+		cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
+		Canny(grayFrame,edges,40,100,3);
+		imshow("source", frame);
+		imshow("gray", grayFrame);
+		imshow("edges", edges);
+		c = waitKey(0);  // waits a key press to advance to the next frame
+		if (c == 27) {
+			// press ESC to exit
+			printf("ESC pressed - capture finished\n"); 
+			break;  //ESC pressed
+		};
+	}
+}
+
+
+void testSnap()
+{
+	VideoCapture cap(0); // open the deafult camera (i.e. the built in web cam)
+	if (!cap.isOpened()) // openenig the video device failed
+	{
+		printf("Cannot open video capture device.\n");
+		return;
+	}
+
+	Mat frame;
+	char numberStr[256];
+	char fileName[256];
+	
+	// video resolution
+	Size capS = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),
+		(int)cap.get(CAP_PROP_FRAME_HEIGHT));
+
+	// Display window
+	const char* WIN_SRC = "Src"; //window for the source frame
+	namedWindow(WIN_SRC, WINDOW_AUTOSIZE);
+	moveWindow(WIN_SRC, 0, 0);
+
+	char c;
+	int frameNum = -1;
+	int frameCount = 0;
+
+
+	namedWindow("Control", WINDOW_AUTOSIZE); //create a window called "Control"
+
+	int iLowH = 0;
+	int iHighH = 179;
+
+	int iLowS = 0;
+	int iHighS = 255;
+
+	int iLowV = 0;
+	int iHighV = 255;
+
+	//Create trackbars in "Control" window
+
+	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179);
+
+
+
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
+
+	createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
+
+	for (;;)
+	{
+		cap >> frame; // get a new frame from camera
+		if (frame.empty())
+		{
+			printf("End of the video file\n");
+			break;
+		}
+
+		Mat_<Vec3b> frame_hsv;
+		cvtColor(frame, frame_hsv, COLOR_BGR2HSV);
+
+		Mat imgThresholded;
+
+		inRange(frame_hsv, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+		//morphological opening (remove small objects from the foreground)
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//morphological closing (fill small holes in the foreground)
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+
+
+
+
+		imshow(WIN_SRC, frame_hsv);
+		++frameNum;
+		
+	//	imshow(WIN_SRC, frame);
+
+		c = waitKey(10);  // waits a key press to advance to the next frame
+		if (c == 27) {
+			// press ESC to exit
+			printf("ESC pressed - capture finished");
+			break;  //ESC pressed
+		}
+		/*
+		if (c == 115){ //'s' pressed - snapp the image to a file
+			frameCount++;
+			fileName[0] = NULL;
+			sprintf(numberStr, "%d", frameCount);
+			strcat(fileName, "Images/A");
+			strcat(fileName, numberStr);
+			strcat(fileName, ".bmp");
+			bool bSuccess = imwrite(fileName, frame);
+			if (!bSuccess) 
+			{
+				printf("Error writing the snapped image\n");
+			}
+			else
+				imshow(WIN_DST, frame);
+		}
+		*/
+	}
+
+}
+
+void MyCallBackFunc(int event, int x, int y, int flags, void* param)
+{
+	//More examples: http://opencvexamples.blogspot.com/2014/01/detect-mouse-clicks-and-moves-on-image.html
+	Mat* src = (Mat*)param;
+	if (event == EVENT_LBUTTONDOWN)
+		{
+			printf("Pos(x,y): %d,%d  Color(RGB): %d,%d,%d\n",
+				x, y,
+				(int)(*src).at<Vec3b>(y, x)[2],
+				(int)(*src).at<Vec3b>(y, x)[1],
+				(int)(*src).at<Vec3b>(y, x)[0]);
+		}
+}
+
+void testMouseClick()
+{
+	Mat src;
+	// Read image from file 
+	char fname[MAX_PATH];
+	while (openFileDlg(fname))
+	{
+		src = imread(fname);
+		//Create a window
+		namedWindow("My Window", 1);
+
+		//set the callback function for any mouse event
+		setMouseCallback("My Window", MyCallBackFunc, &src);
+
+		//show the image
+		imshow("My Window", src);
+
+		// Wait until user press some key
+		waitKey(0);
+	}
+}
+
+int main()
+{
+	testSnap();
+	return 0;
+}
